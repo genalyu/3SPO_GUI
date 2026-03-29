1222fd58df9840d131ece84df1489574e6187813
genalyu <1033130252@qq.com>
2026-03-29 12:22:40 +0800

    add singurity

--- OSWorld/desktop_env/providers/singularity/manager.py
+++ OSWorld/desktop_env/providers/singularity/manager.py
@@ -8,14 +8,25 @@ class SingularityVMManager(VMManager):
     def __init__(self, registry_path=""):
         pass
 
-    def add_vm(self, vm_path):
+    def initialize_registry(self, **kwargs):
         pass
 
-    def check_and_clean(self):
+    def add_vm(self, vm_path, **kwargs):
         pass
 
-    def delete_vm(self, vm_path):
+    def delete_vm(self, vm_path, **kwargs):
         pass
 
-    def initialize_registry(self):
+    def occupy_vm(self, vm_path, pid, **kwargs):
         pass
+
+    def list_free_vms(self, **kwargs):
+        return []
+
+    def check_and_clean(self, **kwargs):
+        pass
+
+    def get_vm_path(self, **kwargs):
+        # For Singularity, we usually pass the path directly via command line
+        # This is a fallback if no path is provided
+        return ""
