83d5c1668affcb0b84d5674acbbc2932049d0875
genalyu <1033130252@qq.com>
2026-04-03 10:15:57 +0800

    sync

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 建议将模型路径设置为环境变量，或者直接在这里修改
-model=${MODEL_PATH:-"/public/home/xlwang/genalyu/models/Qwen2.5-VL-3B-Instruct"}
+model="/public/home/genalyu/models/Qwen2.5-VL-3B"
 model_name=qwen2-5-vl-3b
 num_images=16
 
@@ -19,17 +19,24 @@ trap cleanup SIGINT SIGTERM
 
 # Start processes
 # 设置你想使用的 GPU 数量，默认为 1
-NUM_GPUS=${1:-1}
+NUM_GPUS=8
 
 for i in $(seq 0 $((NUM_GPUS - 1))); do
-    CUDA_VISIBLE_DEVICES=$i python -m vllm.entrypoints.openai.api_server \
+    CUDA_VISIBLE_DEVICES=$i  \
+    VLLM_ALLOW_INVALID_DEVICE=1 \
+    VLLM_DISABLE_MODEL_INSPECT_SUBPROCESS=1 \
+    VLLM_SKIP_PYNVML=1 \
+    python -m vllm.entrypoints.openai.api_server \
         --served-model-name $model_name \
         --model $model \
         --limit-mm-per-prompt image=$num_images \
+        --trust-remote-code \
         --max-model-len 32768 \
         --tensor-parallel-size 1 \
         --port $((9000 + i)) &
 done
+
+
 
 # Wait to keep the script running
 wait
